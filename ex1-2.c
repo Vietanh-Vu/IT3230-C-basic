654acd3a90d9e78314e3f0ae7dafa1f714a4d280
Vietanh-Vu <vuvietanh993@gmail.com>
2022-10-31 17:25:43 +0700

    fix 1.2

--- ex1-2.c
+++ ex1-2.c
@@ -2,17 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
-int array_check (int arr1[100], int arr2[100]) {
-		for (int i = 0; i < 100; i++) {
-			if (arr1[i] != arr2[i]) {
-				return 0;
-			}
+#define SIZE_OF_ARRAY(array) (sizeof(array) / sizeof(array[0]))
+
+int array_check(int *arr1, int *arr2, size_t size1, size_t size2)
+{
+
+	if (size1 != size2)
+		return 0;
+	for (size_t i = 0; i < size1; i++)
+	{
+		if (arr1[i] != arr2[i])
+		{
+			return 0;
 		}
-		return 1;
+	}
+	return 1;
 }
 
-int main () {
-	int arr1[100] = {1,3,4,6};
-	int arr2[100] = {1,3,4};
-	printf("%d", array_check(arr1, arr2));
+int main()
+{
+	int arr1[] = {1, 3, 4, 0};
+	int arr2[] = {1, 3, 4};
+	printf("%d", array_check(arr1, arr2, SIZE_OF_ARRAY(arr1), SIZE_OF_ARRAY(arr2)));
 }
